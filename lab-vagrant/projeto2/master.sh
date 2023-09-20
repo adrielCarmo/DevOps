946b8f8e887c30612fa833252a82b81691b531e4
adrielCarmo <adrieldovale7@gmail.com>
2023-09-20 18:47:45 -0400

    adicionando arquivos do projeto2

--- lab-vagrant/projeto2/master.sh
+++ lab-vagrant/projeto2/master.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker swarm init --advertise-addr 172.16.2.230
-sudo docker swarm join-token worker | grep docker >> ./worker.sh
+sudo docker swarm join-token worker | grep docker >> /vagrant/worker.sh
